ad07c7657ad244f17af8586ea184bf695c9ac59c
senior <senior2508@mail.ru>
2023-09-16 22:20:15 +0700

    не работает js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
 
+    $('.header__btn').on('clik',function (){
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('clik',function (){
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+
 })
